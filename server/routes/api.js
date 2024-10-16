67f3fdf1b13fc05d1ebfe75ed0d71d3067445f40
shigenogoro <kyle990987@gmail.com>
2024-10-16 14:01:44 -0400

    Update the server

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,16 @@
 import express from "express"
+import carsController from "../controllers/carsController.js"
 import featuresController from "../controllers/featuresController.js";
 
 const router = express.Router();
+
+router.get('/cars', carsController.getCars);
+router.get('/cars/:id', carsController.getCarById);
+
+router.post('/cars', carsController.createCar);
+router.put('/cars/:id', carsController.updateCar);
+
+router.delete('/cars/:id', carsController.deleteCar);
 
 // Exteriors
 router.get('/exteriors', featuresController.getAllExteriors);
--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import path from 'path'
 import favicon from 'serve-favicon'
 import dotenv from 'dotenv'
+import cors from 'cors';
 
 // import the router from your routes file
+import router from './routes/api.js'
 
 
 dotenv.config()
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 if (process.env.NODE_ENV === 'development') {
     app.use(favicon(path.resolve('../', 'client', 'public', 'lightning.png')))
@@ -23,7 +26,7 @@ else if (process.env.NODE_ENV === 'production') {
 }
 
 // specify the api path for the server to use
-
+app.use('/api', router)
 
 if (process.env.NODE_ENV === 'production') {
     app.get('/*', (_, res) =>
